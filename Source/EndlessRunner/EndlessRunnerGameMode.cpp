a3f78cdee19eeb6faa690986b29d3e7f4a56f87e
y3lousso <y3lousso@gmail.com>
2017-11-04 12:45:33 -0400

    Setup Project Create 3rd pers runner + animator + inputs Create 90° turn + BP corner

---
+++ Source/EndlessRunner/EndlessRunnerGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "EndlessRunnerGameMode.h"
+#include "EndlessRunnerCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AEndlessRunnerGameMode::AEndlessRunnerGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
